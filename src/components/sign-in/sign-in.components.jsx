6985a2b0003315a4da31f96dab239dfe33cfbc23
Pierre Anisimov <anisim_pierre@outlook.com>
2020-09-14 10:40:58 +0100

    finished styling the Sign in and Google Sig in form

--- src/components/sign-in/sign-in.components.jsx
+++ src/components/sign-in/sign-in.components.jsx
@@ -60,9 +60,10 @@ class SignIn extends React.Component {
             required
           />
           <label>Password</label>
-
+        <div className='buttons'>
           <CustomButton  type="submit">Sign in</CustomButton>
-          <CustomButton  onClick={signInWithGoogle}>{' '}Sign in with Google{' '}</CustomButton>
+          <CustomButton  onClick={signInWithGoogle} isGoogleSignIn> Sign in with Google</CustomButton>
+          </div>
         </form>
       </div>
     );
